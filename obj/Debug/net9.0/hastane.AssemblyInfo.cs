03eec7615b4e89d43c84f6e99fd902bcc6783c8c
omerm09 <omeremlik658@hotmail.com>
2025-05-11 00:29:01 +0300

    onbirinci

--- obj/Debug/net9.0/hastane.AssemblyInfo.cs
+++ obj/Debug/net9.0/hastane.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hastane")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e2131762bd2fff3fac43813e6d7bcd187a70a35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+064116951aaf68636ef77ede8c5b6bd74e68bcac")]
 [assembly: System.Reflection.AssemblyProductAttribute("hastane")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hastane")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
